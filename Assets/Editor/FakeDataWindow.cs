65350d846580f1ae1db7a525101b6e7edabe5a39
Nam Nguyen <hainam125@gmail.com>
2018-09-12 00:04:05 +0700

    Update List

--- Assets/Editor/FakeDataWindow.cs
+++ Assets/Editor/FakeDataWindow.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System;
-
+using System.Reflection;
+using System.Collections.Generic;
+using System.Collections;
 
 public class FakeDataWindow : EditorWindow {
 	[MenuItem("My Game/Fake Data")]
@@ -9,18 +11,29 @@ public class FakeDataWindow : EditorWindow {
 		GetWindow<FakeDataWindow>(false, "Cheats", true);
 	}
 
-	public string objectClass;
-	public bool selfProperty;
-	public bool isEmpty;
-	public object[] fields;
+	private static GUIContent
+		moveButtonContent = new GUIContent("\u21b4", "move down"),
+		duplicateButtonContent = new GUIContent("+", "duplicate"),
+		deleteButtonContent = new GUIContent("-", "duplicate"),
+		addButtonContent = new GUIContent("+", "add element");
+
+	private static GUILayoutOption miniButtonWidth = GUILayout.Width(20f);
+
+	private bool selfProperty;
+	private bool isEmpty;
+	private string objectClass;
+	private Type objectType;
+	private object cachedObject;
+	private PropertyInfo[] propertyInfos;
+	private List<object[]> fields;
+	private IList list;
 
 	public void Awake() {
-		fields = new object[50];
 		isEmpty = true;
 	}
 
 	private void OnGUI() {
-		minSize = new Vector2(300, 300);
+		minSize = new Vector2(300, 600);
 
 		if (isEmpty) {
 			objectClass = EditorGUILayout.TextField("Class", objectClass);
@@ -28,8 +41,13 @@ public class FakeDataWindow : EditorWindow {
 			selfProperty = EditorGUILayout.Toggle("Self Property", selfProperty);
 			EditorGUILayout.BeginHorizontal();
 			if (GUILayout.Button("Generate", GUILayout.Width(100), GUILayout.Height(40)) && !string.IsNullOrEmpty(objectClass)) {
-				var type = typeof(BaseFB).Assembly.GetType(objectClass);
-				if (type != null) isEmpty = false;
+				objectType = typeof(BaseFB).Assembly.GetType(objectClass);
+				if (objectType != null) {
+					isEmpty = false;
+					cachedObject = CreateNewObject();
+					propertyInfos = ((BaseFB)cachedObject).GetProperties(selfProperty);
+					fields = new List<object[]>();
+				}
 				else {
 					Debug.Log("No class with that name");
 				}
@@ -37,47 +55,34 @@ public class FakeDataWindow : EditorWindow {
 			EditorGUILayout.EndHorizontal();
 		}
 		else {
-			var obj = Activator.CreateInstance(typeof(BaseFB).Assembly.GetType(objectClass));
-			var properties = ((BaseFB)obj).GetProperties(selfProperty);
-			for (int i = 0; i < properties.Length; i++) {
-				var p = properties[i];
-				string label = p.Name;
-				if (p.IsType<bool>()) {
-					if (fields[i] == null) fields[i] = false;
-					fields[i] = EditorGUILayout.Toggle(label, bool.Parse(string.Format("{0}",fields[i])));
-				}
-				else if (p.IsType<short>()) {
-					if(fields[i] == null) fields[i] = 0;
-					fields[i] = EditorGUILayout.LongField(label, short.Parse(string.Format("{0}",fields[i])));
-				}
-				else if (p.IsType<int>()) {
-					if(fields[i] == null) fields[i] = 0;
-					fields[i] = EditorGUILayout.IntField(label, int.Parse(string.Format("{0}",fields[i])));
-				}
-				else if (p.IsType<long>()) {
-					if(fields[i] == null) fields[i] = 0;
-					fields[i] = EditorGUILayout.LongField(label, long.Parse(string.Format("{0}",fields[i])));
-				}
-				else if (p.IsType<float>()) {
-					if(fields[i] == null) fields[i] = 0;
-					fields[i] = EditorGUILayout.FloatField(label, float.Parse(string.Format("{0}",fields[i])));
-				}
-				else if (p.IsType<double>()) {
-					if(fields[i] == null) fields[i] = 0;
-					fields[i] = EditorGUILayout.DoubleField(label, double.Parse(string.Format("{0}",fields[i])));
-				}
-				else if (p.IsType<string>()) {
-					if(fields[i] == null) fields[i] = string.Empty;
-					fields[i] = EditorGUILayout.TextField(label, string.Format("{0}",fields[i]));
-				}
+			var listType = typeof(List<>).MakeGenericType(objectType);
+			if (list == null) {
+				list = (IList)Activator.CreateInstance(listType);
 			}
 
-			GUILayout.FlexibleSpace();
+			GUILayout.Space(30);
+
+			for (int n = 0; n < list.Count; n++) {
+				RenderButtons(n);
+				GUILayout.Space(10);
+				if (n > list.Count - 1) break;//remove last element
+				var obj = list[n];
+				for (int i = 0; i < propertyInfos.Length; i++) {
+					RenderField(fields[n], i);
+				}
+
+				GUILayout.Space(15);
+			}
+
+			if (list.Count == 0 && GUILayout.Button(addButtonContent, EditorStyles.miniButton)) {
+				InsertDataAt(0);
+			}
+
+			GUILayout.Space(15);
 
 			EditorGUILayout.BeginHorizontal();
 			if (GUILayout.Button("Save", GUILayout.Width(100), GUILayout.Height(40))) {
-				isEmpty = true;
-				objectClass = string.Empty;
+				Debug.Log("Save Data");
 			}
 			GUILayout.FlexibleSpace();
 			if (GUILayout.Button("Clear", GUILayout.Width(100), GUILayout.Height(40))) {
@@ -85,6 +90,78 @@ public class FakeDataWindow : EditorWindow {
 				objectClass = string.Empty;
 			}
 			EditorGUILayout.EndHorizontal();
+		}
+	}
+
+	private void RenderButtons(int n) {
+		EditorGUILayout.BeginHorizontal();
+		GUILayout.Label("Element " + n, EditorStyles.boldLabel);
+		if (GUILayout.Button(moveButtonContent, EditorStyles.miniButtonLeft, miniButtonWidth) && n + 1 < list.Count) {
+			MoveDownDataAt(n);
+		}
+		if (GUILayout.Button(duplicateButtonContent, EditorStyles.miniButtonMid, miniButtonWidth)) {
+			InsertDataAt(n + 1);
+		}
+		if (GUILayout.Button(deleteButtonContent, EditorStyles.miniButtonRight, miniButtonWidth)) {
+			RemoveDataAt(n);
+		}
+		EditorGUILayout.EndHorizontal();
+	}
+
+	private void MoveDownDataAt(int index) {
+		var dataTemp = list[index + 1];
+		list[index + 1] = list[index];
+		list[index] = dataTemp;
+
+		var fieldTemp = fields[index + 1];
+		fields[index + 1] = fields[index];
+		fields[index] = fieldTemp;
+	}
+
+	private void RemoveDataAt(int index) {
+		list.RemoveAt(index);
+		fields.RemoveAt(index);
+	}
+
+	private void InsertDataAt(int index) {
+		list.Insert(index, CreateNewObject());
+		fields.Insert(index, new object[propertyInfos.Length]);
+	}
+
+	private object CreateNewObject() {
+		return Activator.CreateInstance(objectType);
+	}
+
+	private void RenderField(object[] field, int i) {
+		var p = propertyInfos[i];
+		var label = p.Name;
+		if (p.IsType<bool>()) {
+			if (field[i] == null) field[i] = false;
+			field[i] = EditorGUILayout.Toggle(label, bool.Parse(string.Format("{0}", field[i])));
+		}
+		else if (p.IsType<short>()) {
+			if (field[i] == null) field[i] = 0;
+			field[i] = EditorGUILayout.LongField(label, short.Parse(string.Format("{0}", field[i])));
+		}
+		else if (p.IsType<int>()) {
+			if (field[i] == null) field[i] = 0;
+			field[i] = EditorGUILayout.IntField(label, int.Parse(string.Format("{0}", field[i])));
+		}
+		else if (p.IsType<long>()) {
+			if (field[i] == null) field[i] = 0;
+			field[i] = EditorGUILayout.LongField(label, long.Parse(string.Format("{0}", field[i])));
+		}
+		else if (p.IsType<float>()) {
+			if (field[i] == null) field[i] = 0;
+			field[i] = EditorGUILayout.FloatField(label, float.Parse(string.Format("{0}", field[i])));
+		}
+		else if (p.IsType<double>()) {
+			if (field[i] == null) field[i] = 0;
+			field[i] = EditorGUILayout.DoubleField(label, double.Parse(string.Format("{0}", field[i])));
+		}
+		else if (p.IsType<string>()) {
+			if (field[i] == null) field[i] = string.Empty;
+			field[i] = EditorGUILayout.TextField(label, string.Format("{0}", field[i]));
 		}
 	}
 }
--- Assets/Extra/Helper.cs
+++ Assets/Extra/Helper.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 
 public static class Helper {
-    public static byte[] GetBytes(ArraySegment<byte>? arraySegment)
-    {
-        if (arraySegment.HasValue && arraySegment.Value.Count > 0)
-        {
-            var array = arraySegment.Value;
-            var bytes = new byte[array.Count];
-            Array.Copy(array.Array, array.Offset, bytes, 0, array.Count);
-            return bytes;
-        }
-        return null;
-    }
+	public static byte[] GetBytes(ArraySegment<byte>? arraySegment) {
+		if (arraySegment.HasValue && arraySegment.Value.Count > 0) {
+			var array = arraySegment.Value;
+			var bytes = new byte[array.Count];
+			Array.Copy(array.Array, array.Offset, bytes, 0, array.Count);
+			return bytes;
+		}
+		return null;
+	}
 
-    public static DateTime RandomDate()
-    {
-        var start = new DateTime(1995, 1, 1);
-        int range = (DateTime.Today - start).Days;
-        return start.AddDays(UnityEngine.Random.Range(0, range));
-    }
+	public static DateTime RandomDate() {
+		var start = new DateTime(1995, 1, 1);
+		int range = (DateTime.Today - start).Days;
+		return start.AddDays(UnityEngine.Random.Range(0, range));
+	}
 }
